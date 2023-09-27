6301a4135bb84b8270ea64e972933e71217d0eff
p0k00e0 <Puneet.Kaur@walmart.com>
2023-09-26 18:06:13 -0700

    Adding data binding demo to github

---
+++ app/src/main/java/com/example/databindingexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.databindingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val quoteObj = Quote("This is no test", "Databinding Example")
+
+        binding.quote = quoteObj
+    }
+}
---
+++ app/src/main/java/com/example/databindingexample/Quote.kt
@@ -0,0 +1,3 @@
+package com.example.databindingexample
+
+data class Quote(val text: String, val author: String)
